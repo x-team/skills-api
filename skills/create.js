176410f32241eb648815b825b0d6d9f1848104a2
Karol Sójko <karolsojko@gmail.com>
2017-03-22 12:54:39 +0100

    Add listing skills

--- skills/create.js
+++ skills/create.js
@@ -27,14 +27,12 @@ module.exports.create = (event, context, callback) => {
   };
 
   dynamoDb.put(params, (error, result) => {
-    // handle potential errors
     if (error) {
       console.error(error);
       callback(new Error('Couldn\'t create the skill item.'));
       return;
     }
 
-    // create a response
     const response = {
       statusCode: 200,
       body: JSON.stringify(result.Item),
